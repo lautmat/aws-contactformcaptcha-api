2989ea7893b74509e6299d0724c7bdde23ac5074
Laurent Mathieu <67669940+lautmat@users.noreply.github.com>
2020-09-13 16:10:38 +0700

    first creation

---
+++ src/handlers/index.js
@@ -0,0 +1,59 @@
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+let RECEIVEREMAIL = process.env.RECEIVEREMAIL;
+let SENDEREMAIL = process.env.SENDEREMAIL;
+let RECAPTCHASERVERKEY = process.env.RECAPTCHASERVERKEY;
+
+const fetch = require("node-fetch");
+
+var AWS = require('aws-sdk');
+var ses = new AWS.SES();
+ 
+exports.handler = function (event, context) {
+    checkCaptcha(event, function (err, data) {
+        context.done(err, null);
+    });
+};
+ 
+async function checkCaptcha (event, done) {
+// Validate Captcha
+const recaptchaResult = await fetch(`https://www.google.com/recaptcha/api/siteverify`, {
+    method: 'post',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+    },
+    body: `secret=${RECAPTCHASERVERKEY}&response=${event.rescaptcha}`
+  })
+  .then(res => (res.json()))
+  .then(json => (json.success))
+  .catch(err => { throw new Error(`Error in Google Siteverify API. ${err.message}`) });
+  console.log('Recaptcha result:', recaptchaResult); 	
+  if (event.rescaptcha === null || !recaptchaResult) {
+  throw new Error(`Not a valid captcha response. Probably a bot.`)
+}  
+    
+    var params = {
+        Destination: {
+            ToAddresses: [
+                RECEIVEREMAIL
+            ]
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: 'Name: ' + event.name + '\n\nEmail: ' + event.email + '\n\nSubject: ' + event.subject + '\n\n' + event.desc,
+                    Charset: 'UTF-8'
+                }
+            },
+            Subject: {
+                Data: 'Resume Website Form: ' + event.name,
+                Charset: 'UTF-8'
+            }
+        },
+        Source: SENDEREMAIL
+    };
+    ses.sendEmail(params, done);
+    console.log('Message sent to', RECEIVEREMAIL);
+}
